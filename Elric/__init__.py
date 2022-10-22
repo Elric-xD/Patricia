959a72feb0e4cd307af59d23b64cab37d7fb701a
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-10-22 21:44:59 +0530

    Update __init__.py

--- Elric/__init__.py
+++ Elric/__init__.py
@@ -1,2 +1,16 @@
 import os
 
+print("[INFO] Starting Telethon Client!")
+
+telethon = TelegramClient('innexia', api_id=API_ID, api_hash=API_HASH)
+
+tbot = telethon.start(bot_token=BOT_TOKEN)
+
+print("[INFO] Telethon Client Started!")
+
+updater = tg.Updater(BOT_TOKEN, workers=8, use_context=True)
+
+dispatcher = updater.dispatcher
+
+
+
